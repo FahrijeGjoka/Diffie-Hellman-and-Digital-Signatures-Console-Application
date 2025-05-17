a63783ecd21639ae56632bac047b93b608f0173d
erjonmustafa <erjonmustafa12@gmail.com>
2025-05-17 23:27:40 +0200

    AESutil

---
+++ src/AESEncryptionUtil.java
@@ -0,0 +1,22 @@
+import javax.crypto.*;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+
+public class AESEncryptionUtil {
+    public static String encrypt(String data, byte[] key) throws Exception {
+        SecretKeySpec secretKey = new SecretKeySpec(key, "AES");
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+        byte[] encryptedData = cipher.doFinal(data.getBytes());
+        return Base64.getEncoder().encodeToString(encryptedData);
+    }
+
+    public static String decrypt(String data, byte[] key) throws Exception {
+        SecretKeySpec secretKey = new SecretKeySpec(key, "AES");
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.DECRYPT_MODE, secretKey);
+        byte[] decodedData = Base64.getDecoder().decode(data);
+        byte[] decryptedData = cipher.doFinal(decodedData);
+        return new String(decryptedData);
+    }
+}
