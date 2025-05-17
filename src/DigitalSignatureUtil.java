439d3725e690d9bc0974e491f29ffcf0b9c13c63
FahrijeGjoka <fahrije.gjokiqi@student.uni-pr.edu>
2025-05-17 20:09:28 +0200

    Ndryshime ne klasen DigitalSignatureUtil, si dhe pjesa që merret me krijimin e nënshkrimit dhe dërgimin e tij

--- src/DigitalSignatureUtil.java
+++ src/DigitalSignatureUtil.java
@@ -1,37 +1,25 @@
-
 import java.security.*;
 import java.util.Base64;
 
-
 public class DigitalSignatureUtil {
-    private PrivateKey privateKey;
-    private PublicKey publicKey;
 
 
-    public DigitalSignatureUtil() throws NoSuchAlgorithmException{
-        KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
-        keyGen.initialize(2048);
-        KeyPair pair = keyGen.generateKeyPair();
-        this.privateKey = pair.getPrivate();
-        this.publicKey = pair.getPublic();
-    }
+    public static String createSignature(String data, PrivateKey privateKey) throws Exception {
 
-
-    public String signMessage(String message) throws Exception{
         Signature signature = Signature.getInstance("SHA256withRSA");
         signature.initSign(privateKey);
-        signature.update(message.getBytes());
-        byte [] digitalSignature = signature.sign();
+        signature.update(data.getBytes());
+        byte[] digitalSignature = signature.sign();
         return Base64.getEncoder().encodeToString(digitalSignature);
     }
 
 
-    public static boolean verify(String message, byte[] signatureBytes, PublicKey publicKey) throws Exception {
+    public static boolean verifySignature(String data, String base64Signature, PublicKey publicKey) throws Exception {
+
         Signature signature = Signature.getInstance("SHA256withRSA");
         signature.initVerify(publicKey);
-        signature.update(message.getBytes());
-        return signature.verify(signatureBytes);
+        signature.update(data.getBytes());
+        byte[] digitalSignature = Base64.getDecoder().decode(base64Signature);
+        return signature.verify(digitalSignature);
     }
-
-
 }
--- src/SecureServer.java
+++ src/SecureServer.java
@@ -1,2 +1,53 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.net.*;
+import java.security.*;
+import java.util.Base64;
+
 public class SecureServer {
+    public static void main(String[] args) throws Exception {
+
+        ServerSocket serverSocket = new ServerSocket(5000);
+        System.out.println("Server: Listening on port 5000...");
+        Socket socket = serverSocket.accept();
+        System.out.println("Server: Client connected.");
+
+
+        DiffieHellmanUtil dhServer = new DiffieHellmanUtil();
+
+
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+        out.println(dhServer.getP());
+        out.println(dhServer.getG());
+        out.println(dhServer.getPublicKey());
+
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        BigInteger B = new BigInteger(in.readLine());
+        dhServer.setReceivedPublicKey(B);
+
+        System.out.println("Server: Shared secret established: " + dhServer.getSharedSecret().toString(16));
+
+
+        KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+        keyGen.initialize(2048);
+        KeyPair pair = keyGen.generateKeyPair();
+        PrivateKey privateKeyRSA = pair.getPrivate();
+        PublicKey publicKeyRSA = pair.getPublic();
+
+
+        String welcomeMsg = "Welcome to secure server!";
+
+
+        String digitalSignature = DigitalSignatureUtil.createSignature(welcomeMsg, privateKeyRSA);
+
+
+        out.println(Base64.getEncoder().encodeToString(publicKeyRSA.getEncoded()));
+        out.println(digitalSignature);
+        out.println(welcomeMsg);
+
+
+        socket.close();
+        serverSocket.close();
+    }
 }
